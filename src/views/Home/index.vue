77c7c1d098a1fd7916c32d34c7df0d679bdc3ed2
jingteng <jingteng@soundai.com>
2024-01-03 21:24:22 +0800

    vue学习语法

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,19 @@
+<<<<<<< Updated upstream
 <template>
     <div style="height: 1200px;">我是home页</div>
+=======
+<!-- <template>
+    <div>我是home页</div>
+</template> -->
+
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('Hello World!')
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <input v-model="msg">
+>>>>>>> Stashed changes
 </template>
